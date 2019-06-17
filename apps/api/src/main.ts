c17954011f2a8d1316f963ff0c8b726fb7beea11
anthony <nvhoanganh1909@gmail.com>
2019-06-17 23:40:07 +1000

    add swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,14 +4,28 @@
  **/
 
 import { NestFactory } from '@nestjs/core';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  // custom config
   app.enableCors();
+
+  const options = new DocumentBuilder()
+    .setTitle('API documentation')
+    .setDescription('API documentation')
+    .setVersion('1.0')
+    .addTag('api')
+    .setBasePath('api')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+  // end custom config
 
   const port = process.env.port || 3333;
   await app.listen(port, () => {
