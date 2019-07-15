fd142bc1b561fe8075e4bc84afffee878b9f7f18
anthony nguyen <nvhoanganh1909@gmail.com>
2019-07-15 16:18:48 +1000

    test

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent {
   countries$: Observable<CountryDto[]>;
 
   constructor(private http: HttpClient) {
-    console.log(`Testing`,);
+    console.log(`Testing12`,);
     this.countries$ = this.http.get<CountryDto[]>(`${environment.api_url}countries`);
   }
 }
